6193dcb7367ba2bd70ef22ec0dab6945b12238ea
Mark Burggraf <markb@mantisbible.com>
2016-01-26 22:43:21 -0800

    configuration

---
+++ www/scripts/configuration.js
@@ -0,0 +1,29 @@
+"use strict";
+
+angular.module('config', [])
+
+.constant('ENV', {
+  name: 'development',
+  apiEndpoint: 'http://dev.yoursite.com:10000/',
+  youtubeKey: 'AIzaSyDael5MmCQa1GKQNKQYypmBeB08GATgSEo',
+  ionicPrivateKey: '4205f8ad6fe17a87b37c602b228333d06fdbdbb229f0f0e2',
+  ionicPublicKey: '7caadd30a44e5924a71ff7376357aac569eb073c622e32e2',
+  ionicAppId: 'a3772a71',
+  gcmId: '228071472080',
+  vimeoAccessToken: 'd995ffff0228beba7c9716c3ee0d4add',
+  facebookAppId: '522740477886463',
+  facebookPermanentAccessToken: 'CAANL6xXrSHYBANNHhMUDugVZBHXfVQBMeWG6FmpYROWcOEmC2xze1BNiraZB87NCAZC3w08L7KhCBnhJItZCUzWCgBNzBjt0BkoV6qMoXjIZBjkWRTUGgZBR39OZAiP3DF76jufQ4hJ7xsdQc0l68vFAZAePdZCZAjkjTwaOeEZC22xi8ZAQYBqvNvYRgIfOZBzf4zRURHgrLtNazxzln8ZBkd9FZC7',
+  facebookPage: 'apple.inc',
+  instagramAppId: '2998ca20ed924ca3be22907c6ae77363',
+  firebaseUrl: 'barebone-ionic.firebaseio.com',
+  iosUrl: 'com.titaniumtemplates.barebone-ionic',
+  androidUrl: 'market://details?id=com.titaniumtemplates.barebone-ionic',
+  usesUntilRatePrompt: 4,
+  androidPublisherKey: 'ca-app-pub-3965039466794589/2790557649',
+  iosPublisherKey: 'ca-app-pub-3965039466794589/2930158449',
+  googleAppId: '400671186930-m07eu77bm43tgr30p90k6b9e1qgsva4p.apps.googleusercontent.com',
+  twitterApiKey: 'wXRPbDKzyLXOy4etLq4fNqu8M',
+  twitterApiSecret: '1Bi6DGM98yc9MToSLstGLFaB2tvHOLkBrBBYm8WWI2fTKl0gWX'
+})
+
+;
