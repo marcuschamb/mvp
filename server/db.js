1398db2b77b0f1c58cc38cfbd26789be444a558d
Mark Burggraf <markb@mantisbible.com>
2016-01-27 15:59:55 -0800

    generic db.js to handle CouchDB save document and get document tasks

--- server/db.js
+++ server/db.js
@@ -1,29 +1,50 @@
 var restify = require('restify');
 var q = require('q');
+var PouchDB = require('pouchdb');
 var config = require('./config');
+var db = new PouchDB(config.CouchDBRoot);
 
-var couch = function(req, res, next) {
-  //console.log('couch function...');
-  var client = restify.createClient({
-    url: config.CouchDBRoot
-  });
+var saveDoc = function(doc) {
+  var deferred = q.defer();
 
-  client.get('/db/_design/design_doc/_view/view_name&include_docs=true', function(err, req) {
-    //assert.ifError(err); // connection error
-    res.charSet('utf-8');
-
-    req.on('result', function(err, res1) {
-
-      res1.setEncoding('utf8');
-      res1.on('data', function(chunk) {
-
-        res.write(chunk);
+  db.get(doc._id).then(function(old_doc) {
+    doc._rev = old_doc._rev;
+    return db.put(doc);
+  }).then(function(response) {
+    // handle response
+    //console.log('update response: ' + JSON.stringify(response));
+    deferred.resolve(response);
+  }).catch(function (err) {
+    if (err.status === 404) {
+      db.put(doc).then(function(response){
+        //console.log('new doc response: ' + JSON.stringify(response));
+        deferred.resolve(response);
+      }).catch(function (err) {
+        //console.log('new doc error: ' + JSON.stringify(err));
+        err.step = 'create';
+        deferred.reject(err);
       });
-
-      res1.on('end', function() {
-        res.end();
-      });
-    });
+    } else {
+      //console.log('update error:' + JSON.stringify(err));
+      err.step = 'update';
+      deferred.reject(err);
+    }
   });
-
+  return deferred.promise;
 };
+
+var getDoc = function(_id) {
+  var deferred = q.defer();
+  db.get(_id).then(function (doc) {
+    // handle doc
+    //console.log(JSON.stringify(doc));
+    deferred.resolve(doc);
+  }).catch(function (err) {
+    //console.log('getDoc error: ' + JSON.stringify(err));
+    deferred.reject(err);
+  });
+  return deferred.promise;
+};
+
+exports.saveDoc = saveDoc;
+exports.getDoc = getDoc;
